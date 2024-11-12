f0d5a5f90fce1fa53e442964563afc9695b97dcf
bvnderb <brent.vd.bruggen@gmail.com>
2024-11-12 12:10:36 +0100

    changes to js file | changed HTML index

--- fetch_api/fetch.js
+++ fetch_api/fetch.js
@@ -1,4 +1,33 @@
-const url = 'https://jsonplaceholder.typicode.com/todos';
-fetch(url).then(res => res.json()).then(data => console.log(data)).catch(e => console.log(e));
+// const url = 'https://jsonplaceholder.typicode.com/todos';
+// fetch(url)
+// .then(res => res.json())
+// .then(data => console.log(data))
+// .catch(e => console.log(e));
 
 // HTTP VERB (GET) (POST,DELETE,PUT)
+
+
+// SUBPROBLEMS
+
+// Create a basic HTML structure
+// Create a javascript file with logic
+// Find endpoint API
+// Consume endpoint API
+// Display data in HTML using an iteration
+// make iteration
+// create HTML node (id)
+// node.innerHTML (+=)
+// Analyse array of objects structure (which attributes are available)
+// Done
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        const display = document.getElementById('display');
+        for (let i = 0; i < data.length; i++) {
+            display.innerHTML += (data[i].id + " " + data[i].title + "<br>");
+        }
+
+    })
+    .catch(e => console.log(e));
